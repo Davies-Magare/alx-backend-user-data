5039a0af50a137d6164e9b0d503e55a35c5820b4
Davies Magare <davieskamanda@gmail.com>
2024-11-14 14:20:04 +0000

    First attempt for task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 basic authentication class"""
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -15,4 +16,20 @@ class BasicAuth(Auth):
                 authorization_header,
                 str) and authorization_header.split(' ')[0] == 'Basic':
             return authorization_header.split(' ')[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decode a Base64 string"""
+
+        if isinstance(base64_authorization_header, str):
+            try:
+                decoded_str = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True).decode('utf-8')
+            except Exception:
+                decoded_str = None
+
+            finally:
+                return decoded_str
         return None
