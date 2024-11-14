cf70306fb9ffefd0702605fea897c5465d392080
Davies Magare <davieskamanda@gmail.com>
2024-11-14 12:36:25 +0000

    Create file for basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -34,8 +34,3 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """A stub for the real documentation"""
         return None
-
-
-class BasicAuth(Auth):
-    """The basic authentication class"""
-    pass
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains
+basic authentication class"""
+
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """The basic authentication class"""
+    pass
