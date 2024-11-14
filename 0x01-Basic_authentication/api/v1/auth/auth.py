e1eb30169586fcab71245f18e9b04672ba3922f6
Davies Magare <davieskamanda@gmail.com>
2024-11-14 08:20:19 +0000

    First attempt for task 4: Define paths that need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,21 @@ from typing import List, TypeVar
 class Auth:
     """The authentication class."""
 
+    @staticmethod
+    def slash_handler(path: str) -> str:
+        """Return path equivalent with
+        or without slash"""
+        if path[-1] == '/':
+            return path[:-1]
+        return path + "/"
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """A stub for the real documentation"""
-        return False
+        if path and excluded_paths:
+            slash_alternative = self.slash_handler(path)
+            if path in excluded_paths or slash_alternative in excluded_paths:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """A stub for the real documentation"""
