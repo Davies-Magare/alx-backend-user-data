77a254f924ca40ca0828e91cdf5fd1938890d36d
Davies Magare <davieskamanda@gmail.com>
2024-11-14 12:58:13 +0000

    First attempt for task 7: Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,12 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """The basic authentication class"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extract the Base64 part of the authorization header"""
+        if authorization_header and isinstance(
+                authorization_header,
+                str) and authorization_header.split(' ')[0] == 'Basic':
+            return authorization_header.split(' ')[1]
+        return None
