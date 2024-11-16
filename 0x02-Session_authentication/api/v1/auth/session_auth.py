bacf55e7b40f820afaac9ae51ef49059fd3952b4
Davies Magare <davieskamanda@gmail.com>
2024-11-16 12:44:23 +0000

    First attempt for task 2: Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,9 +7,19 @@ class
 
 from flask import request
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Class to manage user sessions"""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id"""
+
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
