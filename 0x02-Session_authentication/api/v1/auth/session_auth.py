9748295df20ad1c735f92e73ed09e8948ddc37e0
Davies Magare <davieskamanda@gmail.com>
2024-11-16 13:01:05 +0000

    complete first attempt for task 3: User Id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,10 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieve a user Id based on a Session ID"""
+        if not session_id or not isinstance(session_id, str):
+            return None
+        user_id = SessionAuth.user_id_by_session_id.get(session_id)
+        return user_id
