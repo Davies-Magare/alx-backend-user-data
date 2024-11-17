01a19b98aaa8f36a4115c0bc7dc7793408bcf926
Davies Magare <davieskamanda@gmail.com>
2024-11-17 07:11:14 +0000

    First attempt for task 4: Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ user authentication code"""
 
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -34,3 +35,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """A stub for the real documentation"""
         return None
+
+    def session_cookie(self, request=None):
+        """Return a cookie value from a request"""
+        if request is None:
+            return None
+        cookie_name = getenv('SESSION_NAME')
+        cookie = request.cookies.get(cookie_name)
+        return cookie
