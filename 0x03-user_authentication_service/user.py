83e191e39a5d13ef55bc1656865ee044ca3cdb8a
Davies Magare <davieskamanda@gmail.com>
2024-11-20 05:32:02 +0000

    First attempt for task 0: User Model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains code a
+database mapping User
+"""
+
+from sqlalchemy import (
+    Integer,
+    String,
+    ForeignKey,
+    Column
+)
+
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """The user table class mapping"""
+
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
