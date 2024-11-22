b3d6d657225cedffa08c087eb8917acf90114fab
Davies Magare <davieskamanda@gmail.com>
2024-11-22 15:14:46 +0000

    Complete first attempt for task 10: Login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,8 +9,11 @@ from auth import Auth
 from flask import (
     Flask,
     jsonify,
-    request
+    request,
+    abort
 )
+# from db import DB
+# from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
 
@@ -35,6 +38,21 @@ def register_user():
             return jsonify({"email": f"{email}", "message": "user created"})
         except ValueError:
             return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'])
+def login():
+    """Login the user based on their credentials"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    login = AUTH.valid_login(email, password)
+    if not login:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
