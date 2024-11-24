38da74dd0c819e626b9cb66b5ebbad38ca555f76
Davies Magare <davieskamanda@gmail.com>
2024-11-24 17:25:26 +0000

    Try different regex style

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,15 +6,24 @@ Obfuscate a log message string
 import re
 from typing import List
 
+
 def filter_datum(
         fields: List[str],
         redaction: str,
         message: str,
         separator: str) -> str:
-    """Obfuscate a log message"""
+    """Obfuscate a log message
+    Args:
+        fields (list): list of fields to obfuscate
+        redaction (str): what to obfuscate field values to
+        message (str): the log line to obfuscate
+        separator (str): The character separating the fields
+    Return:
+        The obfuscated message
+    """
     for field in fields:
         result = re.sub(
-            f'({field}=)[^{separator}]*',
-            f'{field}={redaction}',
+            field + '=.*?' + separator,
+            field + '=' + redaction + separator,
             message)
     return result
