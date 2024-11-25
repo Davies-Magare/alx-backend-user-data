cd6cdbc5957f408f7cd526fd11fa32120c9c0b7b
Davies Magare <davieskamanda@gmail.com>
2024-11-25 07:58:01 +0000

    First attempt for task 1: log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 """
 Obfuscate a log message string
 """
+import logging
 import re
 from typing import List
 
@@ -22,8 +23,36 @@ def filter_datum(
         The obfuscated message
     """
     for field in fields:
-        result = re.sub(
+        message = re.sub(
             field + '=.*?' + separator,
             field + '=' + redaction + separator,
             message)
-    return result
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """Initialize the object"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter the log string and redact the given fields
+        Args:
+            record (logging.LogRecord):
+               A logging object with all the information pertaining
+               the event being logged
+        """
+        result = super().format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            result,
+            self.SEPARATOR)
